4cce8fef40e379e0034e5a50b61a6d5990e0fa6d
hector <hectorcorcuera09@gmail.com>
2019-04-16 10:10:58 +0200

    Confirmando creacion de la clase ver

---
+++ LosVehiculos/src/Vehiculos/Prueba.java
@@ -0,0 +1,15 @@
+package Vehiculos;
+
+public class Prueba {
+
+	public void ver() {
+		
+	}
+	public static void main(String[] args) {
+		Camion c=new Camion("Rojo", (byte)4, (short)3, (short)120);
+		
+		Motocicleta mc=new Motocicleta("Rojo", (byte)4, (short)90);
+	
+	}
+
+}
